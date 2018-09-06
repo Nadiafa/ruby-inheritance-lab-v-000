9fd3e94881375ddc3aeb3a06eda82b2bb41a0f46
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-06 13:33:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student < User 
-
+  attr_reader    
   def initialize
     @knowledge = []
   end
   
   def learn(string_of_knowledge)
-    @knowledge << string_of_knowledge
+     @knowledge<< string_of_knowledge
   end
 
 end
