69322392cf033b495f998be7fbea60ddc803af83
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-06 13:29:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,14 +2,7 @@ class Student < User
   attr_accessor :knowledge
   @@knowledge = []
   
-  def initialize 
-    
-  end 
   
-  # def learn(knowledge)
-  #   @knowledge = knowledge
-  #   KNOWLEDGE << knowledge
-  # end
 
 
 end
