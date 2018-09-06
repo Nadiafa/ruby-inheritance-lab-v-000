b13ac96ab00ba7068c63f02ef39b3df0cdb6a9df
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-06 13:28:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,16 @@
 class Student < User 
   
+  def initialize 
+    KNOWLEDGE = []
+  end 
+  
+  def learn(knowledge)
+    @knowledge = knowledge
+    KNOWLEDGE << knowledge
+  end
+  
+  def knowledge
+    @knowledge
+  end 
 
 end
